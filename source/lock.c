08ba5750088d316a1c6339b737a16d8fb8d445b2
Luke Griffiths <lukegriffiths@dhcp-vl2042-4264.redrover.cornell.edu>
2022-03-28 15:08:15 -0400

    start of lock.c

---
+++ source/lock.c
@@ -0,0 +1,35 @@
+/*
+ * lock.c
+ *
+ *  Created on: Mar 28, 2022
+ *      Author: lukegriffiths
+ */
+
+
+/**
+ * Initialises the lock structure
+ *
+ * @param l pointer to lock to be initialised
+ */
+void l_init(lock_t* l){
+	//code here
+}
+
+/**
+ * Grab the lock or block the process until lock is available
+ *
+ * @param l pointer to lock to be grabbed
+ */
+void l_lock(lock_t* l){
+	//code here
+}
+
+/**
+ * Release the lock along with the first process that may be waiting on
+ * the lock. This ensures fairness wrt lock acquisition.
+ *
+ * @param l pointer to lock to be unlocked
+ */
+void l_unlock(lock_t* l){
+	//code here
+}
